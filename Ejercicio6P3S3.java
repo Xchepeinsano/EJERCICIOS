6fed3f71f03992da0a711458b023627d4c6e396f
Xchepeinsano <113562357+Xchepeinsano@users.noreply.github.com>
2022-09-16 15:29:52 -0600

    Add files via upload

---
+++ Ejercicio6P3S3.java
@@ -0,0 +1,20 @@
+package ejercicio6p3s3;
+
+import java.util.Scanner;
+
+public class Ejercicio6P3S3 {
+    public static void main(String[] args) {
+        Scanner Reader = new Scanner(System.in);
+        int contador;
+        int fin;
+        System.out.println("por favor ingrese el valor inicial (contador)");
+        contador = Reader.nextInt();
+        System.out.println("Por favor ahora ingrese el valor final");
+        fin = Reader.nextInt();
+        while(contador <= fin){
+        System.out.println("Valor actual de la interación "+contador);
+        contador++;
+        }      
+    }
+    
+}
