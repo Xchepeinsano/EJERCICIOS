13db66786ca958605452ffb5bd3270b9e4683a3a
Xchepeinsano <113562357+Xchepeinsano@users.noreply.github.com>
2022-09-16 14:56:06 -0600

    Add files via upload

--- Periodo2clase4ejercicio2.java
+++ Periodo2clase4ejercicio2.java
@@ -1,28 +1,17 @@
 package com.mycompany.periodo2clase4ejercicio2;
-public class Periodo2clase4ejercicio9 {
+public class Periodo2clase4ejercicio11 {
     public static void main(String[] args) {
-        // programa Java parademostrar switch case
-        // con tipo de datos primitivos (int)
-        int day = 5;
-        String dayString;
-        // instruccion de switch con tipo de datos int
-        dayString = switch (day) {
-            case 1 -> "lunes";
-            case 2 -> "martes";
-            case 3 -> "miercoles";
-            case 4 -> "jueves";
-            case 5 -> "Viernes";
-            case 6 -> "Sabado";
-            case 7 -> "Domingo";
-            default -> "Dia invalido";
-        };   
-        String dayType;
-        
-        dayType = switch (day) {
-            case 1, 2, 3, 4, 5 -> "dia laborable";
-            case 6, 7 -> "fin de semana";
-            default -> "Tipo de dia invalido";
-        }; //multiples cases sin declaraciones break
-        System.out.println(dayString+ " es un " + dayType);
+        // Programa Java para demostrar el uso de un
+        // string para controlar una declaracion switch
+        String str = "dos";
+        switch (str)
+        {
+            case "uno" -> System.out.println("uno");
+            case "dos" -> System.out.println("dos");
+            case "tres" -> System.out.println("tres");
+            default -> {
+            }       
+        }
+        System.out.println("no coincide");
     }
 }
